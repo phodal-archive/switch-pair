f55e054304a46768e201fe28ec14fca703f78129
Fengda HUANG <h@phodal.com>
2014-12-19 16:59:59 +0800

    Add simple json load

--- app/config.js
+++ app/config.js
@@ -2,8 +2,17 @@ require.config({
   // make components more sensible
   // expose jquery
   paths: {
-    "components": "../bower_components",
-    "jquery": "../bower_components/jquery/dist/jquery"
+    //"components": "../bower_components",
+    "jquery": "../bower_components/jquery/dist/jquery",
+    "underscore": "../bower_components/underscore/underscore"
+  },
+  shim: {
+    jquery: {
+      exports: "$"
+    },
+    underscore: {
+      exports: "_"
+    }
   }
 });
 
--- app/main.js
+++ app/main.js
@@ -1,6 +1,22 @@
 // to depend on a bower installed component:
 // define(['bower_components/componentName/file'])
 
-define(["jquery"], function($) {
+define(["jquery", "underscore"], function($, _) {
   $('body').append('jQuery ' + $.fn.jquery + ' loaded!');
+  function appendData(data) {
+    var items = [];
+    $.each(data, function (key, val) {
+      items.push("<li id='" + key + "'>" + val + "</li>");
+    });
+
+    $("<ul/>", {
+      "class": "my-new-list",
+      html: items.join("")
+    }).appendTo("body");
+  }
+
+  $.getJSON("data.json", function (data) {
+    console.log(data);
+    _.each(data, appendData);
+  });
 });
