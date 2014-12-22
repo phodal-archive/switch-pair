c692dd4027bff3e8f4c979255aed0d5c45c99811
Fengda HUANG <h@phodal.com>
2014-12-22 16:55:38 +0800

    Add Random Color

--- app/main.js
+++ app/main.js
@@ -1,4 +1,15 @@
 define(["jquery", "underscore", "role", "switcher", "jStorage"], function($, _, Role, Switcher) {
+    function randomColor(data) {
+        var back = ["blue", "gray", "yellow", "red", "purple"];
+        var rand = back[Math.floor(Math.random() * back.length)];
+        $("." + data["id"]).css('background', rand);
+    }
+
+    function addImage(data) {
+        var img = "<img src=" + data["pic_url"] + "/>";
+        $(img).appendTo("#senior");
+    }
+
     function addSenior(data) {
         var items = [];
         $.each(data, function (key, val) {
@@ -9,13 +20,12 @@ define(["jquery", "underscore", "role", "switcher", "jStorage"], function($, _, 
 
         $("<li/>",
         {
-            "class": "li",
+            "class": data["id"],
             html: items.join("")
         }
         ).appendTo("#senior");
-        //
-        //var img = "<img src=" + data["pic_url"] + "/>";
-        //$(img).appendTo("#senior");
+        randomColor(data);
+        //addImage(data);
     }
 
     function addJunior(data) {
@@ -28,10 +38,11 @@ define(["jquery", "underscore", "role", "switcher", "jStorage"], function($, _, 
 
         $("<li/>",
             {
-                "class": "li",
+                "class": data["id"],
                 html: items.join("")
             }
         ).appendTo("#junior");
+        randomColor(data);
     }
 
     $.jStorage.set("mykey", "keyvalue");
