7f49e4e0e11567cb207c28de818c7e93af5330e8
Fengda HUANG <h@phodal.com>
2014-12-27 10:36:24 +0800

    Add simple test

--- test/tests.js
+++ test/tests.js
@@ -1,17 +1,14 @@
-define(function() {
-  QUnit.start();
+define(function () {
+    QUnit.start();
+    module('Switcher');
 
-  module('jQuery');
-
-  test('is a function', function() {
-    expect(1);
-    stop();
-    require(['jquery'], function($) {
-      start();
-      strictEqual(typeof $, 'function', 'is a function');
+    test('is should be different with last results', function () {
+        stop();
+        require(['jquery', 'libs/Switcher'], function ($, Switcher) {
+            start();
+            var lastResults = [1 ,2 ,3 ,4];
+            var members = [1 ,2 ,3 ,4];
+            notStrictEqual(Switcher.auto(members).withLast(lastResults), lastResults , 'is different');
+        });
     });
-  });
-
-
-
 });
